0af1df605fba329b89bdc0823f3e2dde025a125f
Alina Pshenychna <megasemena@gmail.com>
2024-03-19 00:52:27 +0200

    fix bugs

--- homework5_Search_in _Array.js
+++ homework5_Search_in _Array.js
@@ -1,4 +1,4 @@
-// Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+// 1. Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
 // Знайти суму та кількість позитивних елементів.
 
 const mainArray = [
@@ -6,43 +6,58 @@ const mainArray = [
   76, -4, 12, -35, 4, 47,
 ];
 
-let sumPlus = 0;
-
-mainArray.forEach()
-
-
-
-
-
-let countPositive = 0;
-
-array.forEach((num) => {
-  if (num > 0) {
-    sumPositive += num;
-    countPositive++;
+let sum = 0;
+let quantity = 0;
+mainArray.forEach((item) => {
+  if (item > 0) {
+    sum += item;
+    quantity++;
   }
 });
+console.log("Сумма положительных элементов " + sum);
+console.log(quantity);
 
-console.log("Сума позитивних елементів:", sumPositive);
-console.log("Кількість позитивних елементів:", countPositive);
+// 2. Знайти мінімальний елемент масиву та його порядковий номер.
+let max = mainArray[0];
+let newArrray = mainArray.filter((item) => {
+  if (item > max) {
+    max = item;
+  }
+});
+console.log(max);
+console.log(mainArray.indexOf(max));
 
+// 3. Знайти максимальний елемент масиву та його порядковий номер.
 
-const arr = [20, 40, 77, 53];
+let min = mainArray[0];
+mainArray.filter((item) => {
+  if (item < min) {
+    min = item;
+  }
+});
+console.log(min);
+console.log(mainArray.indexOf(min));
 
-function count(arr) {
-  let sum = 0;
-  arr.forEach(function (item) {
-    sum += item;
-  });
-  return sum;
-}
-count(arr);
+// 4. Визначити кількість негативних елементів.
 
+let quantityNegative = 0;
+mainArray.forEach((item) => {
+  if (item < 0) {
+    quantityNegative++;
+  }
+});
+console.log(quantityNegative);
 
-// Знайти мінімальний елемент масиву та його порядковий номер.
-// Знайти максимальний елемент масиву та його порядковий номер.
-// Визначити кількість негативних елементів.
-// Знайти кількість непарних позитивних елементів.
+// 5. Знайти кількість непарних позитивних елементів.
+
+let elementsQuantity = 0;
+let newwArray = mainArray.filter((item) => {
+  if (item > 0 && item % 2 !== 0) {
+    elementsQuantity++;
+  }
+});
+console.log(elementsQuantity);
+
 // Знайти кількість парних позитивних елементів.
 // Знайти суму парних позитивних елементів.
 // Знайти суму непарних позитивних елементів.
