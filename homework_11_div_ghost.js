1e0222a81d1d0e568f68a2848b9ed46065cbcba3
Alina Pshenychna <megasemena@gmail.com>
2024-04-01 10:19:18 +0300

    complete homework

---
+++ homework_11_div_ghost.js
@@ -0,0 +1,19 @@
+// Є текстове поле на сторінці. Під час фокусування на цьому
+// полі збоку з'являється <div>. При пропажі фокусу
+// - <div> так само пропадає
+
+let divGhost = document.createElement("div");
+
+
+addEventListener('mouseover')
+
+
+
+
+
+
+
+
+addEventListener("mouseover", (event) => {});
+
+onmouseover = (event) => {};
