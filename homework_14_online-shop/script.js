e5e7e52e7d856777930401e8adbff691383511ed
Alina Pshenychna <megasemena@gmail.com>
2024-04-14 10:59:20 +0300

    complete homework

--- homework_14_online-shop/script.js
+++ homework_14_online-shop/script.js
@@ -14,8 +14,30 @@
 // 3. Виводити інформацію про замовлення на сторінку (інформація про товар та про доставку)
 
 const btnBuy = document.querySelector(".btn-buy");
+const btnSend = document.querySelector(".btn-send");
+const informForm = document.querySelector(".inform-form");
+const postNumber = document.querySelector(".post-number");
+const result = document.querySelector(".result");
 
+btnBuy.addEventListener("click", function () {
+  informForm.style.display = "flex";
+});
 
+informForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+  const formObject = {};
 
+  formData.forEach(function (value, key) {
+    formObject[key] = value;
+    result.style.display ="block";
 
-btnBuy.addEventListener("click", function () {});
+    result.innerHTML = "";
+    for (const key in formObject) {
+      const value = formObject[key];
+      const listItem = document.createElement("div");
+      listItem.textContent = `${key}: ${value}`;
+      result.appendChild(listItem);
+    }
+  });
+});
