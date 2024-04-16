ecf394163331bfbb11a5fc3bfe442044dd41ab8f
Alina Pshenychna <megasemena@gmail.com>
2024-04-16 17:05:47 +0300

    complete homework

--- homework_15_reg_exp/script.js
+++ homework_15_reg_exp/script.js
@@ -1,17 +1,3 @@
-//     В інформації товару - кнопка "купити".
-
-//     При натисканні на "купити" нижче з'являється форма оформлення замовлення з наступними полями:
-//     +ПІБ покупця
-//     +Місто (вибір зі списку)
-//     +Склад Нової пошти для надсилання
-//     +Післяплати або оплати банківської картки
-//    + Кількість продукції, що купується
-//     +Коментар до замовлення
-
-// 2. Реалізувати перевірку всіх даних користувача під час підтвердження замовлення - обов'язкові поля заповнені.
-//  Інакше - виводити помилку на сторінку
-
-// 3. Виводити інформацію про замовлення на сторінку (інформація про товар та про доставку)
 
 const btnBuy = document.querySelector(".btn-buy");
 const btnSend = document.querySelector(".btn-send");
@@ -41,3 +27,68 @@ informForm.addEventListener("submit", function (event) {
     }
   });
 });
+
+
+//  +ПІБ
+//     3 слова
+//     кожне з великої букви
+
+// Додати поля (+валідація)
+// 1) +Телефон
+//     від 10 до 13 цифр
+//     можна використовувати лише +,цифри,-ікруглі дужки
+// 2) email (+ валідація):
+//     мінімум 5 символів
+//     наявність @
+//     наявність крапки після @
+
+
+const inputPib = document.querySelector(".pib");
+const error = document.querySelector(".error");
+const patternPib = /^[А-Я]{1}[а-я]{2,}\s[А-Я]{1}[а-я]{2,}\s[А-Я]{1}[а-я]{2,}$/;
+
+
+inputPib.addEventListener("blur", (event) => {
+    const pib = event.target.value;
+    const isPibValid = patternPib.test(pib);
+    if (isPibValid) {
+      error.textContent = "";
+    } else {
+      error.textContent = "Виникла помилка";
+    }
+  });
+
+
+  const inputPhone = document.querySelector(".phone");
+  const errorPhone = document.querySelector(".error-phone");
+  const patternPhone = /^(380)[0-9]{9}$/;
+
+    inputPhone.addEventListener("blur", (event) => {
+      const phone = event.target.value;
+      const isPhoneValid = patternPhone.test(phone);
+      if (isPhoneValid) {
+        errorPhone.textContent = "";
+      }
+       else {
+        errorPhone.textContent = "Виникла помилка";
+      }
+    });
+
+
+
+      const inputEmail = document.querySelector(".email");
+      const errorEmail = document.querySelector(".error-email");
+      const patternEmail = /^[a-z]{3,20}\@[a-z]{1,10}\.[a-z]{1,3}$/;
+
+      inputEmail.addEventListener("blur", (event) => {
+        const email = event.target.value;
+        const isEmailValid = patternEmail.test(email);
+        if (isEmailValid) {
+          errorEmail.textContent = "";
+        } else {
+          errorEmail.textContent = "Виникла помилка";
+        }
+      });
+
+
+
