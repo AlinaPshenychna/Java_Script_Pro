9d68f251773c6986fec22352ccc1b5cc3234dd77
Alina Pshenychna <megasemena@gmail.com>
2024-04-29 14:13:30 +0300

    complete homework

--- homework_16_oop/script.js
+++ homework_16_oop/script.js
@@ -12,7 +12,8 @@
 //     конструктор не потрібен;
 //     масив жителів, який при створенні пустий.
 //     Методи:
-//     додати жителя - метод повинен приймати екземпляр класу Людина, та додавати до масиву жителів.
+//     додати жителя - метод повинен приймати екземпляр класу Людина,
+// та додавати до масиву жителів.
 
 // 3. Створити клас Будинок.
 
@@ -21,7 +22,10 @@
 //     максимальна кількість квартир.
 //     Методи:
 //     конструктор, який приймає один параметр: максимальну кількість квартир;
-//     додати квартиру - метод повинен приймати екземпляр класу Квартира, перевіряти, чи не буде кількість перевищувати максимальну кількість квартир, і якщо це так, додати квартиру, в іншому випадку виводить у консоль відповідне повідомлення.
+//     додати квартиру - метод повинен приймати екземпляр класу Квартира,
+// перевіряти, чи не буде кількість перевищувати максимальну кількість квартир,
+//  і якщо це так, додати квартиру, в іншому випадку виводить у консоль відповідне
+//   повідомлення.
 
 // В якості демонстраціїї створити:
 
@@ -32,16 +36,44 @@
 //     додадити екземпляри класу Квартира до екземплярів класу Будинок.
 
 function human(name, gender) {
-    this.name = name;
-    this.gender = gender;
+  this.name = name;
+  this.gender = gender;
 }
 
 function Flat() {
   this.residents = [];
+  this.addResident = function (human) {
+    this.residents.push(human);
+  };
 }
 
-
-function House() {
+function House(maxApartment) {
   this.apartments = [];
-  this.maxApartment = 
+  this.maxApartment = maxApartment;
+  this.addApartment = function (flat) {
+    if (this.apartments.length < this.maxApartment) {
+      this.apartments.push(flat);
+    } else {
+      console.log("Максимальна кількість квартир");
+    }
+  };
 }
+
+const human1 = new human("Lana", "female");
+const human2 = new human("Taras", "male");
+
+const flat1 = new Flat();
+const flat2 = new Flat();
+
+flat1.addResident(human1);
+flat2.addResident(human2);
+
+const house = new House(5);
+
+house.addApartment(flat1);
+house.addApartment(flat2);
+
+console.log(human1);
+console.log(human2);
+console.log(flat1);
+console.log(flat2);
