a96e638725987226fd14611b9d182a7762070fb6
Alina Pshenychna <megasemena@gmail.com>
2024-06-15 13:16:32 +0300

    added changes

---
+++ Diploma_work/diploma-react-app/src/pages/Login/components/Login.js
@@ -0,0 +1,24 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Login;
