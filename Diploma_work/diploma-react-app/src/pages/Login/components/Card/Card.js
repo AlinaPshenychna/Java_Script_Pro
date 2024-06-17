64d744e480bbcfd130a011b5a19dba462ea353cc
Alina Pshenychna <megasemena@gmail.com>
2024-06-17 13:49:13 +0300

    check

--- Diploma_work/diploma-react-app/src/pages/Login/components/Card/Card.js
+++ Diploma_work/diploma-react-app/src/pages/Login/components/Card/Card.js
@@ -1,7 +1,7 @@
 import "./Card.css";
 import LoginLogo from "../../../../assets/login-logo.svg";
-import Input from "../../../../components/IInput/Input";
-import Button from "../../../../components/Button/Button";
+import Input from "../../../../components/Input/Input";
+import Button from "../../../../components/BButton/Button";
 // import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
 
