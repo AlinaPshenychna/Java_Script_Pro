b42af0be176a39d1fd731557b885cbb7cf328b3c
Alina Pshenychna <megasemena@gmail.com>
2024-07-04 12:36:02 +0300

    compleete hw_27

--- Diploma_work/diploma-react-app/src/components/InputPassword/InputPassword.js
+++ Diploma_work/diploma-react-app/src/components/InputPassword/InputPassword.js
@@ -4,7 +4,7 @@ import { IoEyeOff } from "react-icons/io5";
 import Input from "../Input/Input";
 import React, { useState } from "react";
 
-const InputPassword = () => {
+const InputPassword = ({ value, onChange, onBlur }) => {
   const [isEyeOn, setIsEyeOn] = useState(true);
   const handleClick = () => {
     setIsEyeOn(!isEyeOn);
@@ -12,7 +12,13 @@ const InputPassword = () => {
 
   return (
     <div className="InputPassword-main">
-      <Input type={isEyeOn ? "password" : "text"} placeholder="Password" />
+      <Input
+        type={isEyeOn ? "password" : "text"}
+        placeholder="Password"
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
       <span className="Input-Eye-icon" onClick={handleClick}>
         {isEyeOn ? <IoEye /> : <IoEyeOff />}
       </span>
